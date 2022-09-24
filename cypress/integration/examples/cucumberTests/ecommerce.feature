27aed74281a8452d1a933a82cd174b7be45e218e
gpappU <charismaatic.g@gmail.com>
2022-09-24 07:29:41 -0400

    added tags and BDD code

--- cypress/integration/examples/cucumberTests/ecommerce.feature
+++ cypress/integration/examples/cucumberTests/ecommerce.feature
@@ -1,9 +1,20 @@
 Feature: end to end ecommerce validation
 
     go to url, fill the form, add the products to cart, checkout, select country, and validae success message.
+
+    @Regression
     Scenario: ecommerce product delivery
     Given I open ecommerce page and fill the form
     When products are added to the cart
     And checkout and validate total
     Then I select country and validate success message
+
+    @Smoke
+    Scenario: Navigate to the shop cart page
+    Given I open ecommerce page
+    When I fill the form 
+        |name       |email                 |password  |gender|
+        |John Andrew|john.andrew@gmail.com |Test1234  |Male  |
+    And validate two way binding 
+    Then navigate to product page
     
--- cypress/integration/examples/cucumberTests/ecommerce/ecommerceStepDef.js
+++ cypress/integration/examples/cucumberTests/ecommerce/ecommerceStepDef.js
@@ -9,7 +9,9 @@ const homepage = new HomePage()
 const shopproducts = new ProductPage()
 const checkout = new checkoutPage()
 const purchasebutton = new purchasePage()
+let name
 
+//Scenario 1
 Given('I open ecommerce page and fill the form', function()
 {
     cy.visit(Cypress.env('url'))
@@ -69,4 +71,33 @@ Then('I select country and validate success message', function()
         expect(element.text().includes('Success! Thank you! Your order will be delivered in next few weeks')).to.be.true
     })
 
+})
+
+//Scenatio 2
+Given('I open ecommerce page', function()
+{
+    cy.visit('https://rahulshettyacademy.com/angularpractice')
+})
+
+When ('I fill the form', function(dataTable) 
+{
+    name = dataTable.rawTable[1][0]
+    let email = dataTable.rawTable[1][1]
+    let password = dataTable.rawTable[1][2]
+    let gender = dataTable.rawTable[1][3]
+    homepage.getName().type(name)
+    homepage.getEmail().type(email)
+    homepage.getPassword().type(password)
+    homepage.getGender().select(gender)
+
+})
+
+And('validate two way binding', function()
+{
+    homepage.getTwoWayDataBinding().should('have.value', name)
+})
+
+Then('navigate to product page', function()
+{
+    homepage.getShopTab().click()
 })
